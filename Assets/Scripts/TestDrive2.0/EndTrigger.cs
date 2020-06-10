d4250401680b3a4b7896002cd8e1d9dead2c8266
Frederik <fnienhaus@uos.de>
2020-06-10 11:24:34 +0200

    Made the Scene a little smaller
    Also worked on the scripts.

--- Assets/Scripts/TestDrive2.0/EndTrigger.cs
+++ Assets/Scripts/TestDrive2.0/EndTrigger.cs
@@ -19,18 +19,18 @@ public class EndTrigger : MonoBehaviour
           //other.gameObject.GetComponent<ManualController>().enabled = true;
           //GetComponent<BoxCollider>().enabled = false;
           //StartCoroutine(Triggered(other));
-          Triggered(other);
+          StartCoroutine(Triggered(other));
 
         }
     }
 
-    private void Triggered(Collider other)
+    private IEnumerator Triggered(Collider other)
     {
         //other.gameObject.GetComponent<ManualController>().enabled = false; 
         testEventManager.EndTrigger(other); 
-        Debug.Log("Before waiting...");
-        //yield return new WaitForSeconds(secondsTillManualControl);
-        Debug.Log("After waiting...");
+        Debug.Log("Before waiting End...");
+        yield return new WaitForSecondsRealtime(secondsTillManualControl);
+        Debug.Log("After waiting End...");
         GetComponent<BoxCollider>().enabled = false;
         //other.gameObject.GetComponent<ManualController>().enabled = true;
         testEventManager.ActivateHUD();
---
+++ Assets/Scripts/TestDrive2.0/TurnScreenBlackTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnScreenBlackTrigger : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    private Camera cam;
+
+    [SerializeField] private float time;
+
+    private void Awake()
+    {
+        cam = camera.GetComponent<Camera>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<CarController>())
+        {
+            TurnScreenBlack(time);
+        }
+    }
+
+    private IEnumerator TurnScreenBlack(float time)
+    {
+        cam.clearFlags = CameraClearFlags.SolidColor;
+        cam.cullingMask = Cu
+        
+    }
+}
