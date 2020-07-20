1a536adb7889b38bdb8703ab1e719a123af60b6b
Linus <linus-tiemann@live.de>
2020-07-20 19:50:59 +0200

    missing Nuget data, directinput stuff, and readded chasecam

---
+++ Assets/Scripts/Cars/ManualInput/Steering Wheel/DirectInputTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SharpDX;
+using SharpDX.DirectInput;
+using DeviceType = SharpDX.DirectInput.DeviceType;
+
+public class DirectInputTest : MonoBehaviour
+{
+    private DirectInput _directInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _directInput= new DirectInput();
+
+        _directInput.GetDevices();
+
+        IList<DeviceInstance> devices = new List<DeviceInstance>();
+
+        devices = _directInput.GetDevices();
+        
+        
+        foreach (var device in devices)
+        {
+            if(device.Type == DeviceType.Driving) 
+                Debug.Log(device.ProductName  +" usage "+  device.Usage + " type " + device.Type);
+        }
+        
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+    }
+}
